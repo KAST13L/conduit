f9cc3de2240c2c8b4d9755162bfa88b7ccb122b9
KAST13L <o4enbsmeshno@gmail.com>
2023-06-16 20:40:15 +0300

    feat: add banner.component.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import {Header} from "./common/components/header/header.component.tsx";
+import {Banner} from "./common/components/banner/banner.component.tsx";
 
 export const App = () => {
   return <div>
     <Header/>
+    <Banner/>
   </div>
 };
--- src/common/components/header/header.component.tsx
+++ src/common/components/header/header.component.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Link, NavLink } from "react-router-dom";
 import clsx from "clsx";
+import { Container } from "../container/container.component.tsx";
 
 export const Header: FC = () => {
   const navLinksArray = [
@@ -17,27 +18,29 @@ export const Header: FC = () => {
   return (
     <header>
       <nav className={"px-2 py-4"}>
-        <div
-          className={
-            "max-w-screen-xl mx-auto flex justify-between items-center"
-          }
-        >
-          <Link
-            to={"/"}
-            className={"font-titillium text-2xl text-conduit-green"}
-          >
-            conduit
-          </Link>
-          <ul className={"pl-0 mb-0 list-none flex"}>
-            {navLinksArray.map((el) => (
-              <li key={`nav-array-key${el.title}`}>
-                <NavLink to={el.href} className={navLinkClasses} style={{marginLeft:'15px'}}>
-                  {el.title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Container>
+          <div className={"flex justify-between items-center"}>
+            <Link
+              to={"/"}
+              className={"font-titillium text-2xl text-conduit-green"}
+            >
+              conduit
+            </Link>
+            <ul className={"pl-0 mb-0 list-none flex"}>
+              {navLinksArray.map((el) => (
+                <li key={`nav-array-key${el.title}`}>
+                  <NavLink
+                    to={el.href}
+                    className={navLinkClasses}
+                    style={{ marginLeft: "15px" }}
+                  >
+                    {el.title}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Container>
       </nav>
     </header>
   );
